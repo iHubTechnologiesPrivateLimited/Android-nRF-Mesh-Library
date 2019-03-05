ef8606f47e3415e8d2f630276fda8d5ac39efe5e
tribhuvan <tribhuvan.r@ecoihub.com>
2019-03-05 14:30:14 +0530

    forget password screen

--- Example/nrf-mesh/app/src/main/java/no/nordicsemi/android/nrfmeshprovisioner/LoginActivity.java
+++ Example/nrf-mesh/app/src/main/java/no/nordicsemi/android/nrfmeshprovisioner/LoginActivity.java
@@ -29,7 +29,7 @@ import butterknife.OnClick;
 public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = "LoginActivity";
-    private static final String URL_FOR_LOGIN = "http://144.76.40.143:5000/userlogin/";
+    private static final String URL_FOR_LOGIN = "http://144.76.40.143:5000/userlogin";
     ProgressDialog progressDialog;
     private EditText loginInputEmail, loginInputPassword;
     private Button btnlogin;
@@ -39,7 +39,7 @@ public class LoginActivity extends AppCompatActivity {
     @BindView(R.id.btn_get_password)
     Button btnGetPassword;
     SharedPreferences sharedpreferences;
-    private static final String URL_GET_PWD = "actial url";
+    private static final String URL_GET_PWD = "http://144.76.40.143:5000/forgotpassword";
     private boolean hider = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +49,8 @@ public class LoginActivity extends AppCompatActivity {
         loginInputPassword = (EditText) findViewById(R.id.login_input_password);
         btnlogin = (Button) findViewById(R.id.btn_login);
         btnLinkSignup = (Button) findViewById(R.id.btn_link_signup);
+        btnGetPassword = (Button) findViewById(R.id.btn_get_password);
+        idButtonForgot = (TextView) findViewById(R.id.id_button_forgot);
         // Progress dialog
         progressDialog = new ProgressDialog(this);
         progressDialog.setCancelable(false);
@@ -86,20 +88,108 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
-    }
-    private void getPassword(String s) {
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, URL_GET_PWD, null, new Response.Listener<JSONObject>() {
+
+        //by madhu, enable and disable oparetion for forgot password
+        //start
+        idButtonForgot.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onResponse(JSONObject response) {
+            public void onClick(View view) {
+            Log.d(TAG, "onViewClicked: "+hider);
+            if (hider) {
+
+                loginInputPassword.setVisibility(View.GONE);
+                btnlogin.setVisibility(View.GONE);
+                btnLinkSignup.setVisibility(View.INVISIBLE);
+                btnGetPassword.setVisibility(View.VISIBLE);
+                idButtonForgot.setText("Cancel!");
+                hider = false;
+            } else {
+
+                loginInputPassword.setVisibility(View.VISIBLE);
+                btnlogin.setVisibility(View.VISIBLE);
+                btnLinkSignup.setVisibility(View.VISIBLE);
+                btnGetPassword.setVisibility(View.GONE);
+                idButtonForgot.setText("Forgot Password?");
+
+                hider = true;
+            }
+
+            }
+        });
+        //end
+
+
+
+    }
+    private void getPassword( final String email) {
+        // Tag used to cancel the request
+        Log.d(TAG, "Register Request email: " + email);
+        String cancel_req_tag = "login";
+        progressDialog.setMessage("Logging you in...");
+        showDialog();
+        StringRequest strReq = new StringRequest(Request.Method.POST,
+                URL_GET_PWD, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                // progressDialog.setMessage("onResponse: "+response.toString());
+                //showDialog();
+                Log.d(TAG, "Register Response: " + response.toString());
+                hideDialog();
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    // status code returns 200 when mail is sent successfully
+
+                    if (Integer.parseInt(jObj.getString("status_code")) == 200) {
+
+
+
+                            // Launch Main activity
+                            Intent intent = new Intent(
+                                    LoginActivity.this,
+                                    MainActivity.class);
+
+                            startActivity(intent);
+                            finish();
+
+
+                    } else {
+
+
+                        String errorMsg = jObj.getString("error");
+                        Toast.makeText(getApplicationContext(),
+                                errorMsg, Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+
+                    Toast.makeText(getApplicationContext(),
+                            e.getMessage(), Toast.LENGTH_LONG).show();
+
+                }
 
             }
         }, new Response.ErrorListener() {
+
             @Override
             public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Login Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+                hideDialog();
+            }
+        }) {
 
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting params to login url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("email", email);
+                return params;
             }
 
-        });
+        };
+        // Adding request to request queue
+        AppSingleton.getInstance(getApplicationContext()).addToRequestQueue(strReq,cancel_req_tag);
     }
 
     private void loginUser( final String email, final String password) {
@@ -198,31 +288,7 @@ public class LoginActivity extends AppCompatActivity {
         if (progressDialog.isShowing())
             progressDialog.dismiss();
     }
-    //by madhu, enable and disable oparetion for forgot password
-    //start
-    @OnClick(R.id.id_button_forgot)
-    public void onViewClicked() {
-        if (hider) {
 
-            loginInputPassword.setVisibility(View.GONE);
-            btnlogin.setVisibility(View.GONE);
-            btnLinkSignup.setVisibility(View.INVISIBLE);
-            btnGetPassword.setVisibility(View.VISIBLE);
-            idButtonForgot.setText("Cancle!");
-            hider = false;
-        } else {
-
-            loginInputPassword.setVisibility(View.VISIBLE);
-            btnlogin.setVisibility(View.VISIBLE);
-            btnLinkSignup.setVisibility(View.VISIBLE);
-            btnGetPassword.setVisibility(View.GONE);
-            idButtonForgot.setText("Forgot Password?");
-
-            hider = true;
-        }
-
-    }
-    //end
 }
 
 
